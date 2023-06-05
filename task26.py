9c08919500466bed01afa744e535e7c992ede327
x9316x <100156045+x9316x@users.noreply.github.com>
2023-06-05 11:13:20 +0300

    Add tasks 26 and 28

---
+++ task26.py
@@ -0,0 +1,21 @@
+'''
+Задача 26:
+Напишите программу, которая на вход принимает два числа A и B,
+и возводит число А в целую степень B с помощью рекурсии.
+*Пример:*
+A = 3; B = 5 -> 243 (3⁵)
+    A = 2; B = 3 -> 8 
+'''
+
+def exponentiation(A, B):
+    if B == 0:
+        return 1
+    elif B > 0:
+        return A * exponentiation(A, B - 1)
+    else:
+        return 1 / exponentiation(A, -B)
+
+A = int(input('Введите значение A: '))
+B = int(input('Введите значение B: '))
+
+print(f'{A} в степени {B} равно {exponentiation(A, B)}')
